9e6212d628ea72d1ce9b5322dfc6d8ef9c0e4267
Darssh <darshitsuthar91@gmail.com>
2017-06-29 16:31:53 -0700

    todoapp added

--- todo_list_reducer.js
+++ todo_list_reducer.js
@@ -1,18 +1,86 @@
+const todo = (state, action) => {
+  switch(action.type) {
+    case 'ADD_TODO':
+      return {
+        id: action.id,
+        text: action.text,
+        completed: false
+      };
+    case 'TOGGLE_TODO':
+      if (state.id !== action.id) {
+        return state;
+      }
+
+      return {
+        ...state,
+        completed: !state.completed
+      };
+    default:
+      return state;
+  }
+};
+
 const todos = (state = [], action) => {
   switch (action.type) {
     case 'ADD_TODO':
       return [
         ...state,
-        {
-          id: action.id,
-          text: action.text,
-          completed: false
-        }
+        todo(undefined, action)
       ];
+    case 'TOGGLE_TODO':
+      return state.map(t => todo(t, action));
     default:
       return state;
   }
 };
+
+
+const visibilityFilter = (
+  state = 'SHOW_ALL',
+  action
+) => {
+  switch (action.type) {
+    case 'SET_VISIBILITY_FILTER':
+      return action.filter;
+    default:
+      return state;
+  };
+};
+
+
+// const combineReducers = (reducers) => {
+//   return (state = {}, action) => {
+//     return Object.keys(reducers).reduce(
+//       (nextState, key) => {
+//         nextState[key] = reducers[key](
+//           state[key],
+//           action
+//         );
+//         return nextState;
+//       },
+//       {}
+//     );
+//   };
+// };
+
+const { combineReducers } = Redux;
+const todoApp = combineReducers({
+  todos,
+  visibilityFilter
+});
+
+// const todoApp = (state = {}, action) => {
+//   return {
+//     todos: todos(
+//       state.todos,
+//       action
+//     ),
+//     visibilityFilter: visibilityFilter(
+//       state.visibilityFilter,
+//       action
+//     )
+//   };
+// };
 
 
 const testAddTodo = () => {
@@ -39,5 +107,48 @@ const testAddTodo = () => {
   ).toEqual(stateAfter);
 };
 
+
+const testToggleTodo = () => {
+    const stateBefore = [
+      {
+        id: 0,
+        text: 'Learn Redux',
+        completed: false,
+      },
+      {
+        id: 1,
+        text: 'Go Shopping',
+        completed: false,
+      }
+    ];
+  
+    const action = {
+      type: 'TOGGLE_TODO',
+      id: 1
+    };
+  
+    const stateAfter = [
+      {
+        id: 0,
+        text: 'Learn Redux',
+        completed: false,
+      },
+      {
+        id: 1,
+        text: 'Go Shopping',
+        completed: true,
+      }
+    ];
+  
+    deepFreeze(stateBefore);
+    deepFreeze(action);
+  
+    expect(
+      todos(stateBefore, action)
+    ).toEqual(stateAfter);
+};
+
+
 testAddTodo();
+testToggleTodo();
 console.log('Test Passed');
