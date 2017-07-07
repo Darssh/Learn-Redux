d29afc087115e88f2ffd151d554a7a3fd0a4dfdd
Darssh <darshitsuthar91@gmail.com>
2017-07-07 10:38:07 -0700

    Manage Courses form added

---
+++ PS/src/components/course/ManageCoursePage.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as courseActions from '../../actions/courseActions';
+import CourseForm from './CourseForm';
+
+
+class ManageCoursePage extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      course: Object.assign({}, props.course),
+      errors: {}
+    };
+  }
+
+  render() {
+    return (
+      <CourseForm
+        course={this.state.course}
+        errors={this.state.errors}
+        allAuthors={[]}
+      />
+    );
+  }
+}
+
+
+ManageCoursePage.propTypes = {
+  course: PropTypes.object.isRequired
+};
+
+function mapStateToProps(state, ownProps) {
+  let course = {
+    id: '',
+    watchHref: '',
+    title: '',
+    authorId: '',
+    length: '',
+    category: ''
+  };
+  return {
+    course: course
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(courseActions, dispatch)
+  };
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursePage);
