daaffa92a90334cede1221787b2fd29f1a981bc1
Darssh <darshitsuthar91@gmail.com>
2017-07-13 13:15:27 -0700

    reducer seperated

--- todoapp/src/index.js
+++ todoapp/src/index.js
@@ -1,82 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-var Redux = require('redux');
-
-
-const todo = (state, action) => {
-  switch(action.type) {
-    case 'ADD_TODO':
-      return {
-        id: action.id,
-        text: action.text,
-        completed: false
-      };
-    case 'TOGGLE_TODO':
-      if (state.id !== action.id) {
-        return state;
-      }
-
-      return {
-        ...state,
-        completed: !state.completed
-      };
-    default:
-      return state;
-  }
-}
-
-const todos = (state = [], action) => {
-  switch (action.type) {
-    case 'ADD_TODO':
-      return [
-        ...state,
-        todo(undefined, action)
-      ];
-    case 'TOGGLE_TODO':
-      return state.map(t => todo(t, action));
-    default:
-      return state;
-  }
-};
-
-
-const visibilityFilter = (
-  state = 'SHOW_ALL',
-  action
-) => {
-  switch (action.type) {
-    case 'SET_VISIBILITY_FILTER':
-      return action.filter;
-    default:
-      return state;
-  };
-};
-
-const getVisibleTodos = (
-	todos,
-	filter
-) => {
-	switch(filter) {
-		case 'SHOW_ALL':
-			return todos;
-		case 'SHOW_ACTIVE':
-			return todos.filter(
-				t => !t.completed
-			);
-		case 'SHOW_COMPLETED':
-			return todos.filter(
-				t => t.completed
-			);
-	}
-};
+import { todos, visibilityFilter, getVisibleTodos } from './reducers';
+import { combineReducers, createStore } from 'redux';
 
 const Todo = ({
 	onClick,
 	completed,
 	text
 }) => (
-  <li 
+  <li
   	onClick={onClick}
     style={{
     	textDecoration:
@@ -206,14 +138,11 @@ const Footer = () => (
   </p>
 );
 
-const { combineReducers } = Redux;
 const todoApp = combineReducers({
   todos,
   visibilityFilter
 });
 
-
-const { createStore } = Redux;
 const store = createStore(todoApp);
 
 
@@ -241,12 +170,12 @@ class VisibleTodoList extends React.Component {
 						state.visibilityFilter
 					)
 	      }
-	    	onTodoClick={id => 
+	    	onTodoClick={id =>
 	    		store.dispatch({
 	    			type:'TOGGLE_TODO',
 	    			id
 	    		})
-	    	} 
+	    	}
 	    />
 		);
 	}
