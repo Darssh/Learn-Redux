807a052824838446581d9d50fbb4aa695e4c4d0a
Darssh <darshitsuthar91@gmail.com>
2017-07-17 10:35:41 -0700

    Routes created using react-router 4

--- todoapp/src/components/FilterLink.js
+++ todoapp/src/components/FilterLink.js
@@ -1,37 +1,16 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { setVisibilityFilter } from '../actions';
- 
-const Link = ({ active, children, onClick }) => {
-	if (active) {
-		return <span>{children}</span>
-	}
+import { NavLink } from 'react-router-dom';
 
-	return (
-		<a href='#'
-			onClick={e => {
-				e.preventDefault();
-				onClick();
-			}}
-		>
-			{children}
-		</a>
-	)
-};
+const FilterLink = ({ filter, children }) => (
+  <NavLink
+    exact to={filter === 'all' ? '/' : `/${filter}`}
+    activeStyle={{
+      textDecoration: 'none',
+      color: 'black',
+    }}
+  >
+    {children}
+  </NavLink>
+);
 
-const mapStateToLinkProps = (state, ownProps) => ({
-  active:
-    ownProps.filter ===
-    state.visibilityFilter
-});
-
-const mapDispatchToLinkProps = (dispatch, ownProps) => ({
-  onClick() {
-    dispatch(setVisibilityFilter(ownProps.filter))
-  }
-});
-
-export default connect(
-  mapStateToLinkProps,
-  mapDispatchToLinkProps
-)(Link);
+export default FilterLink;
