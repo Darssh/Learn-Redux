a7bb8fa9783dd9daedb963420feae2021fe277a1
Darssh <darshitsuthar91@gmail.com>
2017-07-17 09:44:36 -0700

    Router added.

--- todoapp/src/index.js
+++ todoapp/src/index.js
@@ -1,27 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import todoApp from './reducers/index';
-import TodoApp from './components/App';
-import { loadState, saveState } from './localStorage';
-import throttle from 'lodash/throttle';
+import configureStore from './configureStore';
+import Root from './components/Root';
 
-const persistedState = loadState();
-const store = createStore(
-  todoApp,
-  persistedState
-);
-
-store.subscribe(throttle(() => {
-  saveState({
-	  todos: store.getState().todos
-  });
-}, 1000));
+const store = configureStore();
 
 ReactDOM.render(
-  <Provider store={store} >
-    <TodoApp />
-  </Provider>,
+  <Root store={store} />,
   document.getElementById('root')
 );
