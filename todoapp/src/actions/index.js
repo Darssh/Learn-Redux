faaae4ff9829cc382d20c05e3008367d5000633a
Darssh <darshitsuthar91@gmail.com>
2017-07-18 18:14:50 -0700

    normilzr added.

--- todoapp/src/actions/index.js
+++ todoapp/src/actions/index.js
@@ -1,3 +1,5 @@
+import { normalize } from 'normalizr';
+import * as schema from './schema';
 import * as api from '../api/';
 import { getIsFetching } from '../reducers';
 
@@ -5,14 +7,17 @@ export const addTodo = (text) => (dispatch) =>
   api.addTodo(text).then(response => {
     dispatch({
       type: 'ADD_TODO_SUCCESS',
-      response,
+      response: normalize(response, schema.todo),
     });
   });
 
-export const toggleTodo = (id) => ({
-  type:'TOGGLE_TODO',
-  id
-});
+export const toggleTodo = (id) => (dispatch) =>
+  api.toggleTodo(id).then(response => {
+    dispatch({
+      type: 'TOGGLE_TODO_SUCCESS',
+      response: normalize(response, schema.todo),
+    })
+  })
 
 export const fetchTodos = (filter) => (dispatch, getState) => {
   if (getIsFetching(getState(), filter)) {
@@ -29,7 +34,7 @@ export const fetchTodos = (filter) => (dispatch, getState) => {
       dispatch({
         type: 'FETCH_TODOS_SUCCESS',
         filter,
-        response,
+        response: normalize(response, schema.arrayOfTodos),
       });
     },
     error => {
